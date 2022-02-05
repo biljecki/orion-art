17ed2f1e62d6b84c300b60f0117c0a11480fad28
Dejan Biljecki <biljecki.dejan@gmail.com>
2022-02-05 13:53:42 +0100

    feat(): initial configurability

--- src/collections/slugcell/slugcell.ts
+++ src/collections/slugcell/slugcell.ts
@@ -9,64 +9,37 @@ const noise3DBlue = makeNoise4D(Date.now() + 10);
 
 const noise2DOffset = makeNoise2D(Date.now());
 
-const backgroundNoise = makeNoise2D(Date.now());
-const noise4D = makeNoise4D(Date.now());
-
-const colors = Object.values({
-  white: { r: 255, g: 255, b: 255, a: 255 },
-  blue: { r: 0, g: 0, b: 255, a: 255 },
-  cyan: { r: 0, g: 255, b: 255, a: 255 },
-  whatever: { r: 255, g: 0, b: 255, a: 255 },
-  yellow: { r: 255, g: 255, b: 0, a: 255 },
-  red: { r: 255, g: 0, b: 0, a: 255 },
-  green: { r: 0, g: 255, b: 0, a: 255 },
-  black: { r: 0, g: 0, b: 0, a: 255 },
-}).map((a) => a);
-
-const numberOfColors = colors.length;
-
 const TWO_PI = 2 * Math.PI;
 
 const nojser = (noise, i, j, woff, zoff, densityQuotint, speedQuotient) => {
- 
   let val =
     noise(
       i / densityQuotint,
       j / densityQuotint,
       woff / speedQuotient,
       zoff / speedQuotient
-    ); //0-2
+    );
 
+  if (val < 0) return 0;
 
-
-  if (val < 0.3) return 0;
-  // return 250;
-
-      // console.log(val)
-
-  if (val < 0.5) return 50 + Math.floor(val * 50) // 0, 1, 2
-  return 150 + Math.floor(val * 50)
-
-  // const color = q  // 150, 200, 250
-
-  // return color;
+  if (val < 0.5) return 100 + 50 + Math.floor(val * 10) // 0, 1, 2
+  return 200 + Math.floor(val * 10)
 };
 
-export const generatePixels = () => {
+export const generatePixels = (config) => {
 
-  // const width = 1280;
-  // const height = 720;
-  // const frames = 100;
+  console.log(config);
+
+  const { zoomQ, offsetSpeed, zoomSpeed, speedQuotient, densityQuotient } = config.slugCell;
+
 
   const frames = 100;
 
   let width = 640;
   let height = 480;
 
-  width = 200;
-  height = 200;
-  
-
+  width = 100;
+  height = 100;
 
   const data: GifData = {
     frames: [],
@@ -77,47 +50,40 @@ export const generatePixels = () => {
     }
   }
 
-  const zoomQ = 2;
-  const zoomSpeed = 1;
-
-
   for (
     let outerAngle = 0, z = 0;
     outerAngle < TWO_PI;
     outerAngle += TWO_PI / frames, z++
   ) {
-    let woff = Math.cos(outerAngle) + 1 ;
-    let zoff = Math.sin(outerAngle) + 1 ;
+    let woff = Math.cos(outerAngle) + 1;
+    let zoff = Math.sin(outerAngle) + 1;
 
+    let redXOffset = noise2DOffset(woff + 123, zoff + 321) * offsetSpeed;
+    let redYOffset = noise2DOffset(woff + 234, zoff + 543) * offsetSpeed;
 
-    const offsetSpeed = 100;
+    let greenXOffset = noise2DOffset(woff + 2552, zoff + 26316) * offsetSpeed;
+    let greenYOffset = noise2DOffset(woff + 4053, zoff + 10403) * offsetSpeed;
 
-    let redXOffset = noise2DOffset(woff + 1000, zoff + 1000) * offsetSpeed;
-    let redYOffset = noise2DOffset(woff + 1000, zoff + 1000) * offsetSpeed;
-
-    let greenXOffset = noise2DOffset(woff, zoff + 1000) * offsetSpeed;
-    let greenYOffset = noise2DOffset(woff + 1000, zoff) * offsetSpeed;
-
-    let blueXOffset = noise2DOffset(woff + 1000, zoff) * offsetSpeed;
-    let blueYOffset = noise2DOffset(woff, zoff + 1000) * offsetSpeed;
+    let blueXOffset = noise2DOffset(woff + 1423, zoff + 30303) * offsetSpeed;
+    let blueYOffset = noise2DOffset(woff + 412453, zoff + 249) * offsetSpeed;
 
     let zoomRed =
-      (noise2DOffset(woff / 5 + 100, zoff / 5 - 100) + 1) * zoomQ; // * 3 //- 3 .. 3
+      (noise2DOffset(woff / zoomSpeed + 100, zoff / zoomSpeed + 200) + 1) * zoomQ; // * 3 //- 3 .. 3
 
     let zoomGreen =
-      (noise2DOffset(woff / 5 + 200, zoff / 5) + 1) * zoomQ;
+      (noise2DOffset(woff / zoomSpeed, zoff / zoomSpeed) + 1) * zoomQ;
 
     let zoomBlue =
-      (noise2DOffset(woff / 5 - 200, zoff / 5) + 1) * zoomQ;
+      (noise2DOffset(woff / zoomSpeed, zoff / zoomSpeed) + 1) * zoomQ;
 
-    let startZoomRedX = (500 * zoomRed) / zoomSpeed;
-    let startZoomRedY = (500 * zoomRed) / zoomSpeed;
+    let startZoomRedX = (height * zoomRed) / -2;
+    let startZoomRedY = (width * zoomRed) / -2;
 
-    let startZoomGreenX = (500 * zoomGreen) / zoomSpeed;
-    let startZoomGreenY = (500 * zoomGreen) / zoomSpeed;
+    let startZoomGreenX = (height * zoomGreen) / -2;
+    let startZoomGreenY = (width * zoomGreen) / -2;
 
-    let startZoomBlueX = (500 * zoomBlue) / zoomSpeed;
-    let startZoomBlueY = (500 * zoomBlue) / zoomSpeed;
+    let startZoomBlueX = (height * zoomBlue) / -2;
+    let startZoomBlueY = (width * zoomBlue) / -2;
 
     const frame: Frame = {
       pixels: []
@@ -127,7 +93,7 @@ export const generatePixels = () => {
 
       for (let x = 0; x < height; x++) {
         const speedQuotient = 1;
-        const densityQuotint = 30;
+        const densityQuotint = 90;
 
         const red = nojser(
           noise3DRed,
@@ -155,7 +121,7 @@ export const generatePixels = () => {
           startZoomBlueY + y * zoomBlue + blueYOffset,
           woff,
           zoff,
-          densityQuotint,
+          densityQuotient,
           speedQuotient
         );
 
@@ -163,7 +129,7 @@ export const generatePixels = () => {
 
         const redC = [red, 0, 0];
         const greenC = [0, green, 0];
-        const blueC = [0, 0, blue];
+        const blueC = [0, 0, blue * 1.5];
 
         const colorZoom = [
           {
@@ -182,14 +148,9 @@ export const generatePixels = () => {
             color: blueC,
           }
         ].sort((a, b) => a.zoom - b.zoom).filter(cz => cz.on).map(a => a.color);
-
-        // if (colorZoom[0] && !colorZoom[1]) {
-
-          // console.log(color)
-          frame.pixels.push({
-            x, y, color: hexer(mergeColors(colorZoom)),
-          })
-    
+        frame.pixels.push({
+          x, y, color: hexer(mergeColors(colorZoom)),
+        })
       }
 
     }
@@ -199,24 +160,10 @@ export const generatePixels = () => {
 
   return data;
 };
-
-
-const mergeColor = (a, b) => {
-  return [
-    a[0] + b[0] / 2,
-    a[1] + b[1] / 2,
-    a[2] + b[2] / 2,
-  ]
-}
-
-
 const mergeColors = (arr) => {
-
-
 
   const len = arr.length;
   let weight = 0
-  // console.log("---------")
 
   const a = arr.reduce((acc: number[], cur: number[], index: number) => {
 
@@ -224,29 +171,19 @@ const mergeColors = (arr) => {
     weight += len - index + 1;
 
     acc[0] += cur[0] * (len - index + 1);
-    acc[1] += cur[1] * (len - index+ 1);
-    acc[2] += cur[2] * (len - index+ 1);
+    acc[1] += cur[1] * (len - index + 1);
+    acc[2] += cur[2] * (len - index + 1);
 
     return acc;
-  }, [0,0,0]);
-
-
-  // console.log([
-  //   a[0]/weight || 0,
-  //   a[1]/weight || 0,
-  //   a[2]/weight || 0,
-  // ])
-
+  }, [0, 0, 0]);
 
   return [
-    Math.round(a[0]/weight || 0),
-    Math.round(a[1]/weight || 0),
-    Math.round(a[2]/weight || 0),
+    Math.round(a[0] / weight || 0),
+    Math.round(a[1] / weight || 0),
+    Math.round(a[2] / weight || 0),
   ]
 }
 
 const hexer = (colors) => {
-  // console.log(colors, colors.map(c => c.toString(16)) )
-
   return `#${colors.map((c: number) => c.toString(16).padStart(2, '0')).join('')}`;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { generatePixels } from "./collections/slugcell/slugcell";
 import { makeGif } from "./utils/gifer/gifer";
+import { getConfig } from "./config/slugcell.config";
 
-const data = generatePixels();
+const config = getConfig();
+
+const data = generatePixels(config);
 
 const pixelSets = data.frames.map(frame => {
   return frame.pixels.map(pixel => pixel.color)
@@ -10,10 +13,7 @@ const pixelSets = data.frames.map(frame => {
 const merged = new Set(pixelSets.flat());
 
 
-console.log("GENERATED PIXELS", [...merged].length);
+console.log("GENERATED PIXELS - NUMBER OF UNIQUE COLORS:", [...merged].length);
 // console.log(data.frames[200])
 
 makeGif(data);
-
-
-
